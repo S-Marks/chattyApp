3e5f80ed19772549d74e83bd1efe4d153f188e10
S-Marks <marks.cherie.art@gmail.com>
2019-07-02 23:59:45 +0000

    Message timeout

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import { props } from 'bluebird';
 
 export default class ChatBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: props.currentUser
+        };
+        this.displayUser = this.displayUser.bind(this);
+    }
+
+    displayUser(event) {
+        this.setState({userName: event.target.defaultValue})
+    }
+
     render() {
         return (
             <footer className="chatbar">
-                <input className="chatbar-username" placeholder="Your Name (Optional)" />
+                <input className="chatbar-username" defaultValue={this.state.userName} placeholder="Your Name (Optional)" />
                 <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
             </footer>
         );
