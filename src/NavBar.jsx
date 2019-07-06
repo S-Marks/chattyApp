9b26fcad8abe60b7d2d915aeda8450f087c4a30b
S-Marks <marks.cherie.art@gmail.com>
2019-07-06 21:19:19 +0000

    Debug

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
 
 function NavBar(props) {
-    if (props.clients === 1) {
-        return (
-            <nav className="navbar">
-                <a href="/" className="navbar-brand">Chatty</a>
-                <div className="userNum">{props.clients} user logged in.</div>
-            </nav>
-        )
-    } else {
-        return (
-            <nav className="navbar">
-                <a href="/" className="navbar-brand">Chatty</a>
-                <div className="userNum">{props.clients} users logged in.</div>
-            </nav>
-        )
-    }
+    return (
+        <nav className="navbar">
+            <a href="/" className="navbar-brand">Chatty</a>
+            <div className="userNum">{props.clients} users logged in.</div>
+        </nav>
+    )
 }
 
 export default NavBar;
